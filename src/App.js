330049c867c50fc0ba2a1adfdaa0dcd2d5105514
chj942 <chj942@gmail.com>
2020-08-13 21:37:42 +0900

    Transferring and reusing values of components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,38 @@ import React from 'react';
 //import './App.css';
 import Hello from './Hello.js';
 
+function TestComp1(comp){
+  console.log(comp);
+  //출력 값
+  //ary: (3) [93, 8, 19]
+  //bool: true
+  //hobby: "book"
+  //name: "CHJ"
+
+return <b>Hi. My name is {comp.name}</b>;
+}
+
+function TestComp2({hobby}){
+  //내부로 바로 접근하기 위해 {}사용 props.hobby == {hobby} 같은 의미
+  console.log(hobby);
+
+return <p>My hobby is {hobby}</p>;
+}
+
+
+
 function App() {
   return (
     <div className="App">
       <h1>Hello~</h1>
       <Hello />
+      <TestComp1 hobby="Reading" name="CHJ" bool={true} ary={[93,8,19]}/>
+      {/* Like component(prop)에 hobby라는 property를 book이라는 value로 설정 */}
+      <TestComp2 hobby="Drawing"/>
+      <TestComp2 hobby="Ex1"/>
+      <TestComp2 hobby="Ex2"/>
+      <TestComp2 hobby="Ex3"/>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //ajax처럼 새로고침없이 요소 생성 -> 소스코드에 처음부터 html을 넣지않으면서 html을 추가하거나 삭제 가능
-//import './index.css';
 import App from './App';
-//import * as serviceWorker from './serviceWorker';
 
 //<App /> == component. react는 component와 함께 동작함.
 //component = HTML을 반환하는 함수. (jsx-JS와 HTML사이의 조합, react만의 유일한 개념. JS안에서 HTML사용)
 
+//컴포넌트를 읽는 객체
+  //react application -> 한번에 하나의 component만 redering 가능
+  //<App /> component 안에서는 여러 component 사용 가능 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root') //root -> index.html의 id명
 );
-//react application -> 한번에 하나의 component만 redering 가능.
+
