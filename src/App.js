eb21b3a0be94eba9ea5c4c92ea1250743586b109
chj942 <chj942@gmail.com>
2020-08-21 18:44:04 +0900

    #2.3 List rendering of objects using map2

--- src/App.js
+++ src/App.js
@@ -21,45 +21,54 @@ function TestComp2({hobby}){
 return <p>My hobby is {hobby}</p>;
 }
 
-//object의 list 가져오기
-//JS의 map 이용-> map = array의 각 item에서 functiond을 실행하는 array를 가질 수 있는 JS function(랜더링). test/map.js파일 참고
 
-function Food({name,picture}){
-return <div>
-        <p>I like {name}<br /></p>
-        <img src={picture}/>
-      </div>;
-}
 
 const foodILike=[
   {
+    id: 1,
     name: "Kimchi",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
   },
   {
+    id: 2,
     name: "Samgyeopsal",
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
   },
   {
+    id: 3,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
   },
   {
+    id: 4,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
   },
   {
+    id: 5,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
   }
 ];
 
+//object의 list 가져오기
+//JS의 map 이용-> map = array의 각 item에서 functiond을 실행하는 array를 가질 수 있는 JS function(랜더링). test/map.js파일 참고
 
+function Food({name,picture}){
+  return <div>
+          <p>I like {name}<br /></p>
+          <img src={picture} alt={name+" 이미지"}/>
+        </div>;
+  }
+
+function renderFood(dish){
+  return <Food name={dish.name} picture={dish.image} key={dish.id}/>
+}
 
 function App() {
   return (
@@ -72,10 +81,16 @@ function App() {
       <TestComp2 hobby="Ex1"/>
       <TestComp2 hobby="Ex2"/>
       <TestComp2 hobby="Ex3"/>
-      <h3>object의 list 가져오기</h3>
-      {/*대괄호안에 있는 text는 JS로 인식. dish===object. foodILike 배열속 각각의 값들을 dish로 받음 */}
-      {foodILike.map(dish=><Food name={dish.name} picture={dish.image}/>)}
 
+      <h3>#2.2 object의 list 가져오기 1- arrow function사용</h3>
+      {/*대괄호안에 있는 text는 JS로 인식. dish===object. foodILike 배열속 각각의 값들을 dish로 받음 */}
+      {foodILike.map(dish=><Food key={dish.id} name=
+      /* key를 추가한 이유 : 요소들의 유일성을 만족 시켜주기 위해 */
+      
+      {dish.name} picture={dish.image}/>)}
+
+      <h3>#2.3 object의 list 가져오기 2</h3>
+      {foodILike.map(renderFood)}
     </div>
   );
 }
